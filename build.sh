8bba999c5fbbc200fc431a9ec41320bcca8c3b6d
noone573 <lienujacnalballiv@gmail.com>
2025-11-27 18:15:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
-python manage.py load_initial_data
 
 # Create admin user if not exists
 python manage.py shell << END
